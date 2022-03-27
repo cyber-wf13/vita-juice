eb05d47c397e393e8164430c9ede1aa384bb1da1
cyber-wf13 <o.zon77788@gmail.com>
2022-03-27 12:16:26 +0300

    Add styles for menu items

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,12 +1,15 @@
 import {createElement} from '../utils';
 
-const createList = (linksArray)=>{
+const createNavList = (linksArray, colorsArray)=>{
   const list = document.createElement("ul");
 
-  linksArray.forEach((link)=>{
+  linksArray.forEach((link, idx)=>{
     let linkElem = document.createElement("a");
     let listItem = document.createElement("li");
     linkElem.textContent = link;
+    if (colorsArray != null || colorsArray != undefined){
+      listItem.style = `--juice-color: #${colorsArray[idx]}`;
+    }
     listItem.prepend(linkElem)
     list.append(listItem);
   })
@@ -25,8 +28,10 @@ cartCount = createElement("span", "cart-count"),
 cabinet = createElement("a", "cabinet"),
 navProducts = createElement("nav", "nav-products"),
 navMenu = createElement("nav", "nav-menu"),
-navProductsList = createList(["соки", "миксы", "смузи", "ласси", "детокс", "боулы", "салаты", "здоровая еда"]),
-navMenuList = createList(["о нас", "франшиза", "контакты", "работа"]);
+navProductsList = createNavList(
+  ["соки", "миксы", "смузи", "ласси", "детокс", "боулы", "салаты", "здоровая еда"], 
+  ["F29114", "FECF3D", "F08AA8", "E5097F", "EF7F1A", "AE4A84", "009846", "7BB933"]),
+navMenuList = createNavList(["о нас", "франшиза", "контакты", "работа"]);
 
 cart.textContent = "Корзина";
 cart.append(cartCount);
