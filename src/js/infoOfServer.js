e6a271b61c8af86a0d99f997e4724e35e1f3de94
cyber-wf13 <o.zon77788@gmail.com>
2022-04-29 15:46:58 +0300

    Add class for header

--- src/js/infoOfServer.js
+++ src/js/infoOfServer.js
@@ -1,111 +1,198 @@
-export const heroImgPaths = ["img/hero/slider-1.png", "img/hero/slider-1.png", "img/hero/slider-1.png"];
-export const productCardInfo = { "соки": "img/products/cards/juice.png", "миксы": "img/products/cards/mix.png", "смузи": "img/products/cards/smoothies.png", "ласси": "img/products/cards/lassy.png", "детокс": "img/products/cards/detox.png", "боулы": "img/products/cards/bouly.png", "салаты": "img/products/cards/salads.png", "здоровая еда": "img/products/cards/eat.png" };
+export const navProductsInfo = [
+  {
+    text: "соки",
+    link: "#",
+    color: "F29114",
+  },
+  {
+    text: "миксы",
+    link: "#",
+    color: "FECF3D",
+  },
+  {
+    text: "смузи",
+    link: "#",
+    color: "F08AA8",
+  },
+  {
+    text: "ласси",
+    link: "#",
+    color: "E5097F",
+  },
+  {
+    text: "детокс",
+    link: "#",
+    color: "EF7F1A",
+  },
+  {
+    text: "боулы",
+    link: "#",
+    color: "AE4A84",
+  },
+  {
+    text: "салаты",
+    link: "#",
+    color: "009846",
+  },
+  {
+    text: "здоровая еда",
+    link: "#",
+    color: "7BB933",
+  },
+];
 
-export const bestsellersCardInfo = [{
-  "title": "ласси",
-  "ml": "500",
-  "price": "375",
-  "imgPath": "img/bestsellers/lassy.png"
-},
-{
-  "title": "микс",
-  "ml": "350",
-  "price": "450",
-  "imgPath": "img/bestsellers/mix.png"
-},
-{
-  "title": "боул",
-  "ml": "350",
-  "price": "300",
-  "imgPath": "img/bestsellers/boul.png"
-},
-{
-  "title": "ласси",
-  "ml": "500",
-  "price": "375",
-  "imgPath": "img/bestsellers/lassy.png"
-}];
+export const navMenuInfo = [
+  {
+    text: "о нас",
+    link: "#",
+  },
+  {
+    text: "франшиза",
+    link: "#",
+  },
+  {
+    text: "контакты",
+    link: "#",
+  },
+  {
+    text: "работа",
+    link: "#",
+  },
+];
+
+export const heroImgPaths = [
+  "img/hero/slider-1.png",
+  "img/hero/slider-1.png",
+  "img/hero/slider-1.png",
+];
+export const productCardInfo = {
+  соки: "img/products/cards/juice.png",
+  миксы: "img/products/cards/mix.png",
+  смузи: "img/products/cards/smoothies.png",
+  ласси: "img/products/cards/lassy.png",
+  детокс: "img/products/cards/detox.png",
+  боулы: "img/products/cards/bouly.png",
+  салаты: "img/products/cards/salads.png",
+  "здоровая еда": "img/products/cards/eat.png",
+};
+
+export const bestsellersCardInfo = [
+  {
+    title: "ласси",
+    ml: "500",
+    price: "375",
+    imgPath: "img/bestsellers/lassy.png",
+  },
+  {
+    title: "микс",
+    ml: "350",
+    price: "450",
+    imgPath: "img/bestsellers/mix.png",
+  },
+  {
+    title: "боул",
+    ml: "350",
+    price: "300",
+    imgPath: "img/bestsellers/boul.png",
+  },
+  {
+    title: "ласси",
+    ml: "500",
+    price: "375",
+    imgPath: "img/bestsellers/lassy.png",
+  },
+];
 
 export const deliveryBlockInfo = {
   deliveryZone: [
     {
-      "color": "#FECF3D",
-      "text": "Минимальная сумма заказа 500 руб."
+      color: "#FECF3D",
+      text: "Минимальная сумма заказа 500 руб.",
     },
     {
-      "color": "#F29114",
-      "text": "Минимальная сумма заказа 750 руб."
+      color: "#F29114",
+      text: "Минимальная сумма заказа 750 руб.",
     },
     {
-      "color": "#F08AA8",
-      "text": "Минимальная сумма заказа 1000 руб."
-    }
+      color: "#F08AA8",
+      text: "Минимальная сумма заказа 1000 руб.",
+    },
   ],
   deliveryPayment: [
     {
-      "imgPath": "img/delivery/cards/money.svg",
-      "title": "наличными",
-      "text": "Оплата наличными курьеру или при получении заказа"
+      imgPath: "img/delivery/cards/money.svg",
+      title: "наличными",
+      text: "Оплата наличными курьеру или при получении заказа",
     },
     {
-      "imgPath": "img/delivery/cards/card.svg",
-      "title": "Картой",
-      "text": "Оплата банковской картой курьеру при получение заказа"
+      imgPath: "img/delivery/cards/card.svg",
+      title: "Картой",
+      text: "Оплата банковской картой курьеру при получение заказа",
     },
     {
-      "imgPath": "img/delivery/cards/online.svg",
-      "title": "Онлайн-оплата",
-      "text": "Безопасная и удобная оплата заказ на сайте"
-    }
+      imgPath: "img/delivery/cards/online.svg",
+      title: "Онлайн-оплата",
+      text: "Безопасная и удобная оплата заказ на сайте",
+    },
   ],
   deliveryWays: [
     {
-      "imgPath": "img/delivery/cards/courier.svg",
-      "title": "курьером",
-      "text": "Вы оплачиваете только заказ, доставка — бесплатно"
+      imgPath: "img/delivery/cards/courier.svg",
+      title: "курьером",
+      text: "Вы оплачиваете только заказ, доставка — бесплатно",
     },
     {
-      "imgPath": "img/delivery/cards/pickup.svg",
-      "title": "самовывоз",
-      "text": "Можете сами забрать заказ, указав это при оформлении заказа."
+      imgPath: "img/delivery/cards/pickup.svg",
+      title: "самовывоз",
+      text: "Можете сами забрать заказ, указав это при оформлении заказа.",
     },
-  ]
-}
+  ],
+};
 
-export const loyaltyListInfo = [{
-  "text": "Всегда рядом в вашем телефоне",
-  "pathImg": "img/loyalty/list/phone.svg"
-},
-{
-  "text": "Участвуй в акциях и получай подарки",
-  "pathImg": "img/loyalty/list/gift.svg"
-}, {
-  "text": "Каждый 5-й напиток бесплатно",
-  "pathImg": "img/loyalty/list/drink.svg"
-}];
+export const loyaltyListInfo = [
+  {
+    text: "Всегда рядом в вашем телефоне",
+    pathImg: "img/loyalty/list/phone.svg",
+  },
+  {
+    text: "Участвуй в акциях и получай подарки",
+    pathImg: "img/loyalty/list/gift.svg",
+  },
+  {
+    text: "Каждый 5-й напиток бесплатно",
+    pathImg: "img/loyalty/list/drink.svg",
+  },
+];
 
 export const footerInfo = {
-  infoText: ["Все права пренадлежат ООО «Компания Вита Джус", "ИНН 12456789/ОГРН123456789833"],
-  infoLink: [{
-    "text": "Публичная оферта",
-    "link": "#"
-  },
-  {
-    "text": "Политика конфиденциальности",
-    "link": "#"
-  }],
-  infoBonus: [{
-    "text": "Пользовательское соглашение",
-    "link": "#"
-  },
-  {
-    "text": "Бонусная программа",
-    "link": "#"
-  }]
-}
+  infoText: [
+    "Все права пренадлежат ООО «Компания Вита Джус",
+    "ИНН 12456789/ОГРН123456789833",
+  ],
+  infoLink: [
+    {
+      text: "Публичная оферта",
+      link: "#",
+    },
+    {
+      text: "Политика конфиденциальности",
+      link: "#",
+    },
+  ],
+  infoBonus: [
+    {
+      text: "Пользовательское соглашение",
+      link: "#",
+    },
+    {
+      text: "Бонусная программа",
+      link: "#",
+    },
+  ],
+};
 
 export const socialInfo = [
-  { "pathImg": "img/social/vk.svg", "link": "#" },
-  { "pathImg": "img/social/instagram.svg", "link": "#" },
-  { "pathImg": "img/social/facebook.svg", "link": "#" }
-]
+  { pathImg: "img/social/vk.svg", link: "#" },
+  { pathImg: "img/social/instagram.svg", link: "#" },
+  { pathImg: "img/social/facebook.svg", link: "#" },
+];
--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,39 @@
-import { body, browserBreakpoint } from './utils';
-import { Navigation, Pagination } from 'swiper';
-import { heroImgPaths, productCardInfo, bestsellersCardInfo, 
-  deliveryBlockInfo, loyaltyListInfo, footerInfo, socialInfo } from './infoOfServer';
+import { body, browserBreakpoint } from "./utils";
+import { Navigation, Pagination } from "swiper";
+import {
+  navProductsInfo,
+  navMenuInfo,
+  heroImgPaths,
+  productCardInfo,
+  bestsellersCardInfo,
+  deliveryBlockInfo,
+  loyaltyListInfo,
+  footerInfo,
+  socialInfo,
+} from "./infoOfServer";
 
-import { Skelet } from './classSkelet';
-import { Carousel } from './classCarousel';
-import { Hero } from './components/hero/classHero';
-import { Production } from './components/production/classProduction';
-import { Bestsellers } from './components/bestsellers/classBestsellers';
-import { Delivery } from './components/delivery/classDelivery';
-import { Loyalty } from './components/loyalty/classLoyalty';
-import { Footer } from './components/base/classFooter';
-import { Social } from './components/base/classSocial';
+import { Skelet } from "./classSkelet";
+import { Carousel } from "./classCarousel";
 
+import { Header } from "./components/base/classHeader";
+import { Hero } from "./components/hero/classHero";
+import { Production } from "./components/production/classProduction";
+import { Bestsellers } from "./components/bestsellers/classBestsellers";
+import { Delivery } from "./components/delivery/classDelivery";
+import { Loyalty } from "./components/loyalty/classLoyalty";
+import { Footer } from "./components/base/classFooter";
+import { Social } from "./components/base/classSocial";
 
 const carouselHeroParams = {
   loop: true,
   modules: [Navigation, Pagination],
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 };
 
@@ -32,8 +42,8 @@ const carouselBestsellersParams = {
   modules: [Navigation],
   slidesPerView: 1,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   breakpoints: {
     1400: {
@@ -41,81 +51,163 @@ const carouselBestsellersParams = {
     },
     992: {
       slidesPerView: 2,
-    }
-  }
+    },
+  },
 };
 
-
-
 const render = () => {
+  const header = new Header("header", "header"),
+    cart = header.createCart(),
+    cabinet = header.createCabinet(),
+    navProducts = new Skelet("nav", "nav-products"),
+    navProductsContent = header.createNavListContent(navProductsInfo),
+    navProductList = header.createList(
+      navProductsContent,
+      "menu-products",
+      "menu-products",
+      true
+    ),
+    navMenu = new Skelet("nav", "nav-menu"),
+    navMenuContent = header.createNavListContent(navMenuInfo),
+    navMenuList = header.createList(
+      navMenuContent,
+      "menu-list",
+      "menu-list",
+      true
+    );
+
+  navProducts.insertItems(navProductList);
+  navMenu.insertItems(navMenuList);
+
+  header.topHeaderWrapper.insertItems([cart, cabinet]);
+  header.bottomHeaderWrapper.insertItems([navProducts, navMenu]);
+
+  const headerContainer = header.createCarcass();
+
   const hero = new Hero("div", "hero");
   const heroCarousel = new Carousel(carouselHeroParams, "swiper-hero");
   heroCarousel.createSlide(hero.createSliderImg(heroImgPaths));
   hero.insertItems(heroCarousel.carouselSkelet);
 
   const production = new Production("наша продукция", "products");
-  production.sectionWrapper.insertItems(production.getProductCard(productCardInfo));
+  production.sectionWrapper.insertItems(
+    production.getProductCard(productCardInfo)
+  );
 
   const bestsellers = new Bestsellers("хиты продаж", "bestsellers");
-  const bestsellersCarousel = new Carousel(carouselBestsellersParams, "swiper-bestsellers");
+  const bestsellersCarousel = new Carousel(
+    carouselBestsellersParams,
+    "swiper-bestsellers"
+  );
   bestsellers.sectionWrapper.insertItems(bestsellersCarousel.carouselSkelet);
-  bestsellersCarousel.createSlide(bestsellers.getBestsellersCard(bestsellersCardInfo));
+  bestsellersCarousel.createSlide(
+    bestsellers.getBestsellersCard(bestsellersCardInfo)
+  );
 
   const delivery = new Delivery("доставка и оплата", "delivery");
-  const deliveryBlockZones = delivery.setDeliveryContent("Зона доставки",
-    delivery.createDeliveryZones(deliveryBlockInfo.deliveryZone)),
-    deliveryBlockPayment = delivery.setDeliveryContent("варианты оплаты",
-      delivery.createDeliveryOptions(deliveryBlockInfo.deliveryPayment)),
-    deliveryBlockWays = delivery.setDeliveryContent("способы доставки",
-      delivery.createDeliveryOptions(deliveryBlockInfo.deliveryWays));
+  const deliveryBlockZones = delivery.setDeliveryContent(
+      "Зона доставки",
+      delivery.createDeliveryZones(deliveryBlockInfo.deliveryZone)
+    ),
+    deliveryBlockPayment = delivery.setDeliveryContent(
+      "варианты оплаты",
+      delivery.createDeliveryOptions(deliveryBlockInfo.deliveryPayment)
+    ),
+    deliveryBlockWays = delivery.setDeliveryContent(
+      "способы доставки",
+      delivery.createDeliveryOptions(deliveryBlockInfo.deliveryWays)
+    );
 
   const deliveryBlockZonesMap = new Skelet("img", "delivery__block-map");
   deliveryBlockZonesMap.setAttr("src", "img/delivery/map.jpg");
   deliveryBlockZones.insertItems(deliveryBlockZonesMap);
 
-  delivery.sectionWrapper.insertItems([deliveryBlockZones, deliveryBlockPayment, deliveryBlockWays]);
+  delivery.sectionWrapper.insertItems([
+    deliveryBlockZones,
+    deliveryBlockPayment,
+    deliveryBlockWays,
+  ]);
 
   const loyalty = new Loyalty("Наша система лояльности", "loyalty"),
-
     loyaltyListContent = loyalty.createListContent(loyaltyListInfo),
-    loyaltyList = loyalty.createList(loyaltyListContent, "loyalty-list", "loyalty", true),
-
+    loyaltyList = loyalty.createList(
+      loyaltyListContent,
+      "loyalty-list",
+      "loyalty",
+      true
+    ),
     loyaltyBlockText = loyalty.createBlockText(),
-    loyaltyBlockContent = loyalty.createContentBlockInner([loyalty.subTitle, loyaltyList, loyaltyBlockText], "div", "loyalty__content"),
-
+    loyaltyBlockContent = loyalty.createContentBlockInner(
+      [loyalty.subTitle, loyaltyList, loyaltyBlockText],
+      "div",
+      "loyalty__content"
+    ),
     loyaltyCard = loyalty.createCard(),
-    loyaltyWrapperInner = loyalty.createContentBlockInner([loyaltyCard, loyaltyBlockContent], "div", "loyalty__wrapper-inner");
+    loyaltyWrapperInner = loyalty.createContentBlockInner(
+      [loyaltyCard, loyaltyBlockContent],
+      "div",
+      "loyalty__wrapper-inner"
+    );
 
   loyalty.container.insertItems(loyaltyWrapperInner);
   loyalty.sectionWrapper.insertItems(loyalty.container);
 
   const footer = new Footer("footer", "footer"),
+    footerListInfoContent = footer.createListInfoContent(footerInfo.infoText),
+    footerListInfo = footer.createList(
+      footerListInfoContent,
+      "list-info",
+      false,
+      true,
+      "footer__list-info"
+    ),
+    footerListLinksContent = footer.createListContent(footerInfo.infoLink),
+    footerListLinks = footer.createList(
+      footerListLinksContent,
+      "list-links",
+      false,
+      true,
+      "footer__list-links"
+    ),
+    footerListBonusContent = footer.createListContent(footerInfo.infoBonus),
+    footerListBonus = footer.createList(
+      footerListBonusContent,
+      "list-links",
+      false,
+      true,
+      "footer__list-links"
+    );
 
-  footerListInfoContent = footer.createListInfoContent(footerInfo.infoText),
-  footerListInfo = footer.createList(footerListInfoContent, "list-info", false, true, "footer__list-info"),
-
-  footerListLinksContent = footer.createListContent(footerInfo.infoLink),
-  footerListLinks = footer.createList(footerListLinksContent, "list-links", false, true, "footer__list-links"),
-
-  footerListBonusContent = footer.createListContent(footerInfo.infoBonus),
-  footerListBonus = footer.createList(footerListBonusContent, "list-links", false, true, "footer__list-links");
-
-  footer.listWrapper.insertItems([footerListInfo, footerListLinks, footerListBonus]);
+  footer.listWrapper.insertItems([
+    footerListInfo,
+    footerListLinks,
+    footerListBonus,
+  ]);
 
   const social = new Social("div", "social");
   social.createSocialLinks(socialInfo);
   const socialCollected = social.collectSocial();
 
-  body.prepend(hero.elem, production.collectElements(), bestsellers.collectElements(), delivery.collectElements(),
-    loyalty.collectElements(), footer.collectFooter(socialCollected));
+  body.prepend(
+    headerContainer.elem,
+    hero.elem,
+    production.collectElements(),
+    bestsellers.collectElements(),
+    delivery.collectElements(),
+    loyalty.collectElements(),
+    footer.collectFooter(socialCollected)
+  );
 
   heroCarousel.init();
   bestsellersCarousel.init();
 
-
-  if (browserBreakpoint == "md" || browserBreakpoint == "sm" || browserBreakpoint == "xs") {
+  if (
+    browserBreakpoint == "md" ||
+    browserBreakpoint == "sm" ||
+    browserBreakpoint == "xs"
+  ) {
     bestsellersCarousel.swiperObj.navigation.destroy();
   }
-}
+};
 
 render();
